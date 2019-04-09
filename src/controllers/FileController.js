493c6d045aada44e475887cae2f9e1b80722003d
Raphael Ponce <raphafleury123@gmail.com>
2019-04-09 14:17:58 -0300

    Listagem no banco de dados

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,10 +1,21 @@
 const File = require("../models/File");
+const Box = require("../models/Box");
 
 class FileController{
     async store(req,res){
-        console.log(req.file);
+        const box = await Box.findById(req.params.id);
 
-        return res.send("OK");
+        const file = await File.create({
+            title: req.file.originalname,
+            path : req.file.key,
+        });
+
+        box.files.push(file);
+
+        await box.save();
+
+        
+        return res.json(file);
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,10 @@ const FileController = require("./controllers/FileController");
 
 
 routes.post("/boxes", BoxController.store);
-routes.post("/files", multer(multerConfig).single("file"), FileController.store)
+routes.get("/boxes/:id", BoxController.show);
+
+
+
+routes.post("/boxes/:id/files", multer(multerConfig).single("file"), FileController.store)
 
 module.exports = routes;
